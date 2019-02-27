e10e6ad81513034b6cce816c898b49d0782ba5c0
Fernando Salles Claro <fsclaro@gmail.com>
2019-02-27 20:41:24 -0300

    criação das migrations

---
+++ database/migrations/2019_02_27_232306_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+	    $table->string('descricao', 100);
+	    $table->integer('qtd');
+	    $table->float('prc_venda', 8, 2);
+	    $table->float('prc_compra', 8, 2);
+	    $table->integer('providers_id');
+	    $table->integer('classifications_id');
+            $table->timestamps();
+
+	    $table->foreign('providers_id')->references('id')->on('providers');
+	    $table->foreign('classifications_id')->references('id)->on('classifications');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
