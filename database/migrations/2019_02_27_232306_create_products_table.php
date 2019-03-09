ac3ccdce3778dd9c9954c7eb1e60e280bfc42885
fernando <fsclaro@gmail.com>
2019-03-08 23:02:18 -0300

    atualização 2019.03.08

--- database/migrations/2019_02_27_232306_create_products_table.php
+++ database/migrations/2019_02_27_232306_create_products_table.php
@@ -19,12 +19,12 @@ class CreateProductsTable extends Migration
 	        $table->integer('qtd');
 	        $table->float('prc_venda', 8, 2);
 	        $table->float('prc_compra', 8, 2);
-	        $table->integer('providers_id');
-	        $table->integer('classifications_id');
+	        $table->integer('providers_id')->unsigned();
+	        $table->integer('classifications_id')->unsigned();
             $table->timestamps();
 
 	        $table->foreign('providers_id')->references('id')->on('providers');
-	        $table->foreign('classifications_id')->references('id)->on('classifications');
+	        $table->foreign('classifications_id')->references('id')->on('classifications');
         });
     }
 
